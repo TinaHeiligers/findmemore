f6ddc4ee043331610d11416cd0c1c5b930729843
TinaHeiligers <tina.heiligers@gmail.com>
2018-05-01 12:58:16 -0700

    Adds player redux tests

---
+++ src/ducks/player/playerActions.spec.js
@@ -0,0 +1,11 @@
+import playerActions from 'ducks/player/playerActions.js';
+describe ('>>ACTION -- Test playerActions', () => {
+  it('++actionCreator addPlayer', () => {
+    const testName = 'Name';
+    const testAddPlayer = playerActions.addPlayer(testName);
+    expect(testAddPlayer).toEqual({
+      type: playerActions.ADD_PLAYER,
+      name: testName,
+    });
+  });
+});
