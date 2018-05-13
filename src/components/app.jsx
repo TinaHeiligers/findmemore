0804e3b6614b28038eab688d775689b8d08afc2a
TinaHeiligers <tina.heiligers@gmail.com>
2018-05-13 16:42:07 -0700

    Pushes to startPage on load

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import { ImmutableFragment } from 'redux-little-router';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { push, ImmutableFragment } from 'redux-little-router';
 import styled from 'styled-components';
 import Help from 'components/help.jsx';
 import StartPage from 'components/startPage/StartPage.jsx';
 import GameLayout from 'components/game/gameLayout.jsx';
 import img from 'redux/cards/images/table.jpg';
 
-const App = styled.div`
+const AppDiv = styled.div`
   height: 100vh;
   width: 100vw;
   background-image: url(${img});
   background-size: cover;
 `;
-export default () =>
-  <App>
-    <ImmutableFragment forRoute='/startPage'>
-      <StartPage />
-    </ImmutableFragment>
-    <ImmutableFragment forRoute='/game'>
-      <GameLayout />
-    </ImmutableFragment>
-    <ImmutableFragment forRoute='/help'>
-      <Help />
-    </ImmutableFragment>
-  </App>;
+
+class App extends Component {
+  componentDidMount() {
+    this.props.push({ pathname: '/startPage' });
+  }
+  render() {
+    return (
+      <AppDiv>
+        <ImmutableFragment forRoute='/startPage'>
+          <StartPage />
+        </ImmutableFragment>
+        <ImmutableFragment forRoute='/game'>
+          <GameLayout />
+        </ImmutableFragment>
+        <ImmutableFragment forRoute='/help'>
+          <Help />
+        </ImmutableFragment>
+      </AppDiv>
+    );
+  }
+}
+export default connect(null, { push })(App);
