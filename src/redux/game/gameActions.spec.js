574f9cfe5d4d651205e05b4a065d1477c83953b6
TinaHeiligers <tina.heiligers@gmail.com>
2018-05-28 16:44:42 -0700

    Adds game action tests

--- src/redux/game/gameActions.spec.js
+++ src/redux/game/gameActions.spec.js
@@ -9,4 +9,16 @@ describe ('game action creators -> gameActions', () => {
       level: testLevel,
     });
   });
+  it('startNextTurn starts a new game turn', () => {
+    const testStartNextTurn = gameActions.startNextTurn();
+    expect(testStartNextTurn).toEqual({ type: gameActions.START_NEXT_TURN });
+  });
+  it('switchTurns switches between game turns', () => {
+    const testSwitchTurns = gameActions.switchTurns();
+    expect(testSwitchTurns).toEqual({ type: gameActions.SWITCH_TURNS });
+  });
+  it('playerTurn issues a PLAYER_TURN action', () => {
+    const testPlayerTurn = gameActions.playerTurn();
+    expect(testPlayerTurn).toEqual({ type: gameActions.PLAYER_TURN });
+  });
 });
