492fc5af5392b3e76498cc2a7e9e2000dbe9987e
TinaHeiligers <tina.heiligers@gmail.com>
2018-06-24 14:35:06 -0700

    Enables random shuffling for deploy

--- src/redux/cards/cardsServices.js
+++ src/redux/cards/cardsServices.js
@@ -1,39 +1,33 @@
 import cards from 'redux/cards/cardsData';
-
 const levelToNum = {
   easy: 6,
   medium: 8,
   hard: 12,
 };
-
 export const getCards = (level) => {
   const cardsFromApi = cards;
   const result = randomizeCards(cardsFromApi, levelToNum[level])
   return result;
-}
-
+};
 const randomizeCards = (cardsFromApi, num) => {
-    //doubles each card and randomizes order
-    const newCards = cardsFromApi.slice(0, num).concat(cardsFromApi.slice(0, num)).map(card => Object.assign({}, card));//making dups
-    const shuffledCards = shuffle(newCards);
-    //Resetting selected prop to false on repressing the button
-    const cardsWithProps = shuffledCards.map(card => {
-      card.status = 'hidden';
-      card.selected = false;
-      card.matched = false;
-      return card;
-    });
-    return [...cardsWithProps];
-}
-
+  const newCards = cardsFromApi.slice(0, num).concat(cardsFromApi.slice(0, num)).map(card => Object.assign({}, card));
+  const shuffledCards = shuffle(newCards);
+  const cardsWithProps = shuffledCards.map(card => {
+    card.status = 'hidden';
+    card.selected = false;
+    card.matched = false;
+    return card;
+  });
+  return [...cardsWithProps];
+};
 const shuffle = (arr) => {
   let currentIndex = arr.length, temporaryValue, randomIndex;
-  // while (0 !== currentIndex) {
-  //   randomIndex = Math.floor(Math.random() * currentIndex);
-  //   currentIndex -= 1;
-  //   temporaryValue = arr[currentIndex];
-  //   arr[currentIndex] = arr[randomIndex];
-  //   arr[randomIndex] = temporaryValue;
-  // }
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = arr[currentIndex];
+    arr[currentIndex] = arr[randomIndex];
+    arr[randomIndex] = temporaryValue;
+  }
   return arr;
 };
