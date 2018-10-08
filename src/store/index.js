aa06b7d67f4d62851b2e62f53e9c83af69b63105
tinaheiligers <tinaheiligers@gmail.com>
2018-10-08 08:42:34 -0700

    Updates jest

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
 import { applyMiddleware, createStore } from 'redux';
-import Immutable from 'immutable';
 import { combineReducers } from 'redux-immutable';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
@@ -7,7 +6,7 @@ import { immutableRouterForBrowser } from 'redux-little-router';
 import reducers from 'redux/rootReducers';
 import rootSaga from 'redux/rootSagas';
 
-export default function () {
+export default () => {
   const routes = {
     '/startPage' : {
       title: 'Start',
@@ -19,22 +18,21 @@ export default function () {
       title: 'Help',
     },
   };
-
-  const { reducer: routerReducer, enhancer, middleware: routerMiddleware } = immutableRouterForBrowser({ routes });
-
+  const { 
+    reducer: routerReducer, 
+    enhancer, 
+    middleware: routerMiddleware,
+  } = immutableRouterForBrowser({ routes });
   const sagaMiddleware = createSagaMiddleware();
   const middlewares = [routerMiddleware, sagaMiddleware];
-
   const allReducers = {
     ...reducers,
     router: routerReducer
   };
-
-  const initialState = Immutable.Map({});
   const store = createStore(
     combineReducers(allReducers),
     composeWithDevTools(enhancer, applyMiddleware(...middlewares))
   );
   sagaMiddleware.run(rootSaga);
   return store;
-}
+};
